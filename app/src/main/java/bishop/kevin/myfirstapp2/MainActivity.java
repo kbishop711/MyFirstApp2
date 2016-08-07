1b184713a631d2d9265659c608de7d4b323670f5
Kevin Bishop <ktb2012@att.net>
2016-08-07 18:40:19 -0500

    Testing changes. Added textbox to send to new activity.

--- app/src/main/java/bishop/kevin/myfirstapp2/MainActivity.java
+++ app/src/main/java/bishop/kevin/myfirstapp2/MainActivity.java
@@ -1,7 +1,15 @@
 package bishop.kevin.myfirstapp2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra("STRING_I_NEED", message);
+        startActivity(intent);
     }
 }
